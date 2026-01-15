ac1d799e4248e0884b1d0dfafac4becd0b12411e
JerryBruce <jerry.r.bruce@gmail.com>
2026-01-15 15:27:12 -0600

    created config file to manage multiple tables

---
+++ pipeline/src/config.py
@@ -0,0 +1,16 @@
+MIDDLE = 306
+
+BOOK_MAP = {
+    "skills": {
+        "segments": [
+            {"page": 4, "bbox": (MIDDLE, 410, 612, 760)},
+            {"page": 5, "bbox": (0, 30, MIDDLE, 760)}
+        ],
+        "join_char": " ",
+        "pattern": r"^([\w\s]+):\s*(.*)",
+        "pattern_type": "key_value",
+        "raw_name": "skills_raw.txt",
+        "clean_name": "skills_clean.json"
+    },
+
+}
